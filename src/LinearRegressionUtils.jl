ee15713fcac0e9022f795af78f1bd75534c77a4d
roger <roger.herikstad@gmail.com>
2024-03-26 14:28:34 +0800

    Adds more tests and dedicated type

--- src/LinearRegressionUtils.jl
+++ src/LinearRegressionUtils.jl
@@ -5,7 +5,19 @@ using LinearAlgebra
 using StatsBase
 using Random
 
-export llsq_stats
+export llsq_stats, LinRegStats
+
+struct LinRegStats{T<:Real}
+    X::Matrix{T}
+    y::Vector{T}
+    β::Vector{T}
+    Δβ::Vector{T}
+    σ::T
+    r²::T
+    pv::T
+    rss::T
+
+end
 
 """
 ```julia
@@ -14,18 +26,68 @@ function llsq_stats(X,y;kvs...debug)
 Least square regression using `MultivariateStats.llsq`, also returning r² and p-value of the fit, computed via F-test
 ```
 """
-function llsq_stats(X::Matrix{T},y::Vector{T};kvs...) where T <: Real
-	β = llsq(X, y)
-	p1 = length(β)
-	n = length(y)
-	prt = X*β[1:end-1] .+ β[end]
-	rsst = sum(abs2, y .- mean(y))
-	rss1 = sum(abs2, y .- prt)
-	F = (rsst - rss1)/(p1-1)
-	F /= rss1/(n-p1)
-	pv = 1.0 - cdf(FDist(p1-1, n-p1), F)
-	r² = 1.0 - rss1/rsst
-	β, r², pv, rss1
+function llsq_stats(X::Matrix{T},y::Vector{T};do_interactions=false,do_stats=false, kvs...) where T <: Real
+    n,d = size(X)
+    if do_interactions
+        # add all pairwise interactions
+        nterms = div(d*(d-1),2)
+        Xi = zeros(T, n, nterms)
+        k = 1
+        for i in 1:d-1
+            for j in i+1:d
+                Xi[:,k] = X[:,i].*X[:,j]
+                k += 1
+            end
+        end
+        return llsq_stats([X Xi], y;do_interactions=false, do_stats=do_stats, kvs...)
+    end
+	β = llsq(X, y;kvs...)
+    prt = X*β[1:end-1] .+ β[end]
+    rsst = sum(abs2, y .- mean(y))
+    rss1 = sum(abs2, y .- prt)
+    r² = 1.0 - rss1/rsst
+    if do_stats
+        # use non-parametric stats to establish significance. Basically, run the regression 1000 times, each
+        # time shuffling the relationship between X and y. The overall regression if significant if the actual rss is smaller than that of the shuffle, and each coefficient can be considered significant if it is outside the 5th to the 95th percentile.
+        βs = fill(0.0, length(β), 1000)
+        pc = fill(0.0, length(β))
+        rsss = fill(0.0, 1000)
+        for i in 1:size(βs,2)
+            βs[:,i] = llsq(X, shuffle(y);kvs...)
+            prts = X*βs[1:end-1,i] .+ βs[end,i]
+            rsss[i] = sum(abs2, y .- prts)
+        end
+        for i in 1:length(β)
+            _βs = βs[i,:]
+            sort!(_βs)
+            idx = searchsortedfirst(_βs,β[i])
+            pc[i] = (idx-1)/999
+        end
+        sort!(rsss)
+        idx = searchsortedfirst(rsss, rss1)
+        pc_rss = (idx-1)/999
+        pv = NaN
+    else
+        pc = fill(NaN, length(β))
+        pc_rss = NaN
+        p1 = length(β)
+        n = length(y)
+        if length(β) > size(X,2)
+            prt = X*β[1:end-1] .+ β[end]
+        else
+            prt = X*β
+        end
+        σ = sqrt(rss1/(n-p1))
+        V = inv(X'X)
+        pc = sqrt.(diag(V)).*σ
+        pc_rss = σ
+        F = (rsst - rss1)/(p1-1)
+        F /= rss1/(n-p1)
+        pv = 1.0 - cdf(FDist(p1-1, n-p1), F)
+        r² = 1.0 - rss1/rsst
+    end
+	β, r², pv, rss1, pc, pc_rss
+    LinRegStats(X,y,β,pc,pc_rss,r², pv,rss1)
 end
 
 adjusted_r²(r²::Float64, n::Int64, p::Real) = 1.0 - (1.0-r²)*(n-1)/(n-p)
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,38 @@ using StableRNGs
     a,b = (1.3, 0.5)
     x = range(0,stop=1.0, length=20)
     y = b .+ a*x + 0.1*randn(rng, 20)
-    β, r², pv, rss1 = llsq_stats(repeat(x,1,1), y)
-    @test β ≈ [1.3693138524189115, 0.43924187315363106]
-    @test r² ≈ 0.9574226025153478
-    @test pv ≈ 8.693046282814976e-14
-    @test rss1 ≈ 0.1536016506153289
+    results = llsq_stats(repeat(x,1,1), y)
+    
+    @test results.β ≈ [1.3693138524189115, 0.43924187315363106]
+    @test results.r² ≈ 0.9574226025153478
+    @test results.pv ≈ 8.693046282814976e-14
+    @test results.rss ≈ 0.1536016506153289
+    @test results.Δβ ≈ [0.03531561821080929]
+    @test results.σ ≈ 0.09237653941442567
+end
+
+@testset "Interaction" begin
+    rng = StableRNG(1234)
+    a,b,c,d = (1.3, 0.5, 0.2, 0.1)
+    x1 = randn(rng, 20)
+    x2 = randn(rng, 20)
+    y = b .+ a*x1 .+ c*x2 .+ d*x1.*x2 .+ 0.1*randn(rng, 20)
+    results = llsq_stats([x1 x2], y;do_interactions=true)
+    @test results.β ≈  [1.27337061123587, 0.14505516134295823, 0.026233140664101238, 0.4465079643898126]
+    @test results.r² ≈ 0.9956057228435399
+    @test results.pv < 1e-10
+    @test results.rss ≈ 0.11241220401856507 
+end
+
+@testset "Non-parametric stats" begin
+    rng = StableRNG(1234)
+    a,b = (1.3, 0.5)
+    x = randn(rng, 20)
+    y = b .+ a*x + 0.1*randn(rng, 20)
+    results = llsq_stats(repeat(x,1,1), y;do_stats=true)
+    @show results.β
+    @show results.r²
+    @show results.rss
+    @show results.Δβ
+    @show results.σ 
 end
